d9fe6a9d2d8d07cad8e3857dab033d95a0be228d
kashmeera3011 <131755283+kashmeera3011@users.noreply.github.com>
2023-05-06 17:54:26 +0530

    Add files via upload

---
+++ Module 2/MyBiodata.java
@@ -0,0 +1,15 @@
+package automation1;
+
+public class MyBiodata {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is KAshmeera");
+		
+		System.out.println("My Birth DAte is 30 november 1993");
+		
+		System.out.println("My Age is 29 year old");
+		
+		System.out.println("B-404, iscon ambito appartment,80ft ring road, rajkot");
+	}
+}
---
+++ Module 2/SumOfTwoNumbers.java
@@ -0,0 +1,22 @@
+package automation1;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 50 ;
+		int b = 10;
+		
+		int c = a + b;
+		int d = a - b;
+		int e = a / b;
+		int f = a * b;
+		int g = a % b ;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+}
