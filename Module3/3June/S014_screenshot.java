1e3a866e12ab194495d1b44ccbd79bdbfec631a5
kashmeera3011 <131755283+kashmeera3011@users.noreply.github.com>
2023-06-13 19:53:53 +0530

    Add files via upload

--- Module3/3June/S014_screenshot.java
+++ Module3/3June/S014_screenshot.java
@@ -3,38 +3,54 @@ package task_3_june;
 import java.io.File;
 import java.io.IOException;
 
-
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+/*
+ * public class S014_screenshot { public static void getScreenShot(WebDriver
+ * driver, String imgname) {
+ * 
+ * TakesScreenshot ts = (TakesScreenshot) driver;
+ * 
+ * File source = ts.getScreenshotAs(OutputType.FILE);
+ * 
+ * File dest = new File("F:\\img\\" + imgname+ ".png");
+ * 
+ * try { Files.copy(source, dest); System.out.println("screenshot taken"); }
+ * catch (IOException e) { // TODO Auto-generated catch block
+ * e.printStackTrace(); } }
+ * 
+ * public static void main(String[] args) throws InterruptedException {
+ * 
+ * WebDriver driver =
+ * DriverConnection.getconnection("https://www.facebook.com/");
+ * getScreenShot(driver, "err"); } }
+ */
 
-import com.google.common.io.Files;
+public class S014_screenshot {
 
-public class S014_screenshot 
-{
-	public static void getScreenShot(WebDriver driver, String imgname)
-	{
+	public static void getScreenShot(WebDriver driver, String imgname) {
 		
 		TakesScreenshot ts = (TakesScreenshot) driver;
 
 		File source = ts.getScreenshotAs(OutputType.FILE);
 		
-		File dest = new File("E:\\img\\" + imgname+ ".png");
+		File dest = new File("D:\\img\\"+imgname+".png");
 
-		try 
-		{
-			Files.copy(source, dest);
+		try {
+			FileUtils.copyFile(source, dest);
 			System.out.println("screenshot taken");
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) throws InterruptedException 
-	{
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = DriverConnection.getconnection("https://www.facebook.com/");
 		getScreenShot(driver, "err");
