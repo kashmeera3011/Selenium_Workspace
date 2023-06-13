75f05b4f9e66330eb427e6964945a8005dcb6b7e
kashmeera3011 <131755283+kashmeera3011@users.noreply.github.com>
2023-06-13 19:55:03 +0530

    Add files via upload

--- Module3/6June/DynamicTableEx.java
+++ Module3/6June/DynamicTableEx.java
@@ -1,17 +1,25 @@
 package task_6_june;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 import task_3_june.DriverConnection;
 
 public class DynamicTableEx 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		WebDriver driver = DriverConnection.getconnection("https://www.techlistic.com/p/demo-selenium-practice.html");
 		
-		String text = driver.findElement(By.xpath("//table[@id=\"post-body-1325137018292710854\"]/div[1]/div[1]/div[5]/table/tbody/tr[3]/td[1]")).getText();
+		JavascriptExecutor js  =(JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		
+		driver.manage().window().maximize();
+		
+		String text = driver.findElement(By.xpath("//table[@id=\"post-body-1325137018292710854\"]/div[1]/div[1]/div[5]/table")).getText();
+		Thread.sleep(2000);
 		System.out.println(text);
+		
 	}
 }
